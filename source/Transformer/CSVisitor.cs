5b61ac5308ff6cd937defcdb790f4c891733e439
Anmol Sinha <anmolsinha1201@outlook.com>
2017-10-12 12:36:45 +0530

    Added other visitors to properly convert to CS

--- source/Transformer/CSVisitor.cs
+++ source/Transformer/CSVisitor.cs
@@ -9,19 +9,27 @@ namespace AHKCoreCompile
 	{
 		public override variableDeclarationClass variableDeclaration(variableDeclarationClass context)
 		{
-			context.extraInfo = new extraInfoDelegate(() => $"dynamic {context.variableName}");
+			context.extraInfo = new extraInfoDelegate(() => $"dynamic {context.variableName};");
 			return context;
 		}
 
 		public override functionDeclarationClass functionDeclaration(functionDeclarationClass context)
 		{
-			context.extraInfo = new extraInfoDelegate(() => $"{context.functionHead}\n{{\n\t{context.functionBody.FlattenExtraInfo("\n\t")}\n}}");
+			context.extraInfo = new extraInfoDelegate(() => 
+			$"{(context.returnType == null ? "void" : context.returnType)} {context.functionHead}\n{{\n\t{context.functionBody.FlattenExtraInfo("\n").Indent<string>()}\n}}");
 			return context;
 		}
 
 		public override classDeclarationClass classDeclaration(classDeclarationClass context)
 		{
-			context.extraInfo = new extraInfoDelegate(() => $"class {context.className}\n{{\n\t{context.classBody.FlattenExtraInfo("\n\t")}\n}}");
+			context.extraInfo = new extraInfoDelegate(() => 
+			$"class {context.className}\n{{\n\t{context.classBody.FlattenExtraInfo("\n").Indent<string>()}\n}}");
+			return context;
+		}
+
+		public override variableAssignClass variableAssign(variableAssignClass context)
+		{
+			context.extraInfo = new extraInfoDelegate(() => context.ToString() + ";");
 			return context;
 		}
 	}
