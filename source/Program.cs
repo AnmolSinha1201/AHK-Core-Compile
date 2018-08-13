b04f6b7633d73211c075e506ab8c1139fc13c258
Anmol Sinha <anmolsinha1201@outlook.com>
2018-08-13 14:47:08 +0530

    Removed everything for fresh start

--- source/Program.cs
+++ source/Program.cs
@@ -8,10 +8,7 @@ namespace source
 		static void Main(string[] args)
 		{
 			var parserInstance = new Parser();
-			var AHKTree = parserInstance.parse("var1=456\nclass asd{var=123\nvar2=890}\nvar2=321");
-			
-			var asd = new AHKCoreCompile.Transformer();
-			asd.Test(AHKTree);
+			var AHKTree = parserInstance.parse("var:=123\n");
 		}
 	}
 }
